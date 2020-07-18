b4a9e4b5e97e0a8ad7cce14ec8eb29417bf691fe
Prashanth B <beeps@google.com>
2020-07-18 13:00:36 +0530

    React components
    * Menu component
    * State of the component for passing data

---
+++ nerb/react/confusion/setup.md
@@ -0,0 +1,28 @@
+# Setup
+
+* [Yarn](https://yarnpkg.com/) instead of NPM
+```
+$ npm install -g yarn
+```
+* Create react app
+```
+$ npx create-react-app@1.5.2 confusion --scripts-version=react-scripts@1.1.4
+```
+OR
+```
+$ npx create-react-app confusion
+```
+And then
+```
+$ cd confusion && yarn start
+```
+
+## React-strap
+
+Install
+```
+$ yarn add bootstrap@4.0.0
+$ yarn add reactstrap@5.0.0 react-popper@0.9.2
+```
+
+Configure vscode to set language as "Javascript React"
--- nerb/react/confusion/src/App.js
+++ nerb/react/confusion/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 import './App.css';
 
+// React compoent
+// What is state? what is view? 
+// Manu/ just imports the view of the imported ManuComponent
+// This whole thing is rendered by index.js as a single App component
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        
+        <Menu />
       </div>
     );
   }
